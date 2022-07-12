bc21876f9133ef9a4b2a4852d15d653e78f71df9
Gabriel Andres <gabrielleoandres@gmail.com>
2022-07-12 12:57:25 -0500

    Created function

--- public/index.js
+++ public/index.js
@@ -1,1 +1,11 @@
-
+const strictEquals = function (a, b) {
+  let result = Object.is(a, b);
+  if (Number.isNaN(a) || Number.isNaN(a)) {
+    return (result = false);
+  }
+  if (!a && !b) {
+    result = true;
+  }
+  return result;
+};
+console.log(strictEquals());
